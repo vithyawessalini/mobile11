1df654ddb9297718f482805a3c0aa673aeed3be5
vithyawessalini <118716389+vithyawessalini@users.noreply.github.com>
2024-02-06 13:33:32 +0530

    now

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 import colors from "colors";
 const connectDB = async () => {
   try {
-   const conn = await mongoose.connect('mongodb+srv://Vithyawessalini:7Ty1Nlwmpt05br4g@cluster0.95wjl6y.mongodb.net/?retryWrites=true&w=majority');
+  //  const conn = await mongoose.connect('mongodb+srv://Vithyawessalini:7Ty1Nlwmpt05br4g@cluster0.95wjl6y.mongodb.net/?retryWrites=true&w=majority');
+  const conn = await mongoose.connect('mongodb+srv://Vithyawessalini:7Ty1Nlwmpt05br4g@cluster0.95wjl6y.mongodb.net/mobile');
     console.log(
       `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
     );
