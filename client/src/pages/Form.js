84af48f9223d704d4ca6a594153b5acda2fdecd1
vithyawessalini <118716389+vithyawessalini@users.noreply.github.com>
2024-02-13 19:19:51 +0530

    now

--- client/src/pages/Form.js
+++ client/src/pages/Form.js
@@ -3,55 +3,42 @@ import Layout from "./../components/Layout/Layout";
 
 import '../styles/Form.css'
 
-export default function Form()
-{
-    return(<>
-    <Layout>
-        <br/><br/><br/>
-        <h3 className="text-center">Reserve Party Hall here!!</h3>
+export default function Form() {
+    return (
+        <>
+            <Layout>
+                <br /><br /><br />
+                <h3 className="text-center">Book Mobile Service Here</h3>
 
-        <div id="feedback-form">
-              <h2 class="header">Reservation Form</h2>
-              <div>
-                <form action="https://formspree.io/f/meqwdjkz" method="POST">
-                <label class="pure-material-textfield-standard">
-                      <input placeholder="Your Name"/>
-                      {/* <span>Your Name</span> */}
-                </label> 
-                <label class="pure-material-textfield-standard"> 
-                <input type="number" class="pure-material-textfield-standard" name="date" placeholder="Members Count"/>
-                      {/* <span>Select Date</span> */}
-                </label> 
-                <label class="pure-material-textfield-standard"> 
-                <input type="email" class="pure-material-textfield-standard" name="date" placeholder="Your Email"/>
-                      {/* <span>Select Date</span> */}
-                </label> 
-                <label class="pure-material-textfield-standard"> 
-                <input type="number" class="pure-material-textfield-standard" name="date" placeholder="Your Mobile Number"/>
-                      {/* <span>Select Date</span> */}
-                </label> 
-                <br></br>
-                <label class="pure-material-textfield-standard"> 
-                <input type="date" class="pure-material-textfield-standard" name="date" placeholder="Pick a Date"/>
-                      {/* <span>Select Date</span> */}
-                </label> 
-
-
-                {/* <label class="pure-material-textfield-standard">
-                      <input placeholder=" "/>
-                      <span>About Customizing halls</span>
-                </label><br/>                  
-                  <label for="feedback-notify">Remind me about event via SMS</label>
-                  <input type="checkbox" id="feedback-notify" name="notify"/>
-                  <input type="text" id="feedback-phone" name="phone" placeholder="Phone number"></input> */}
-                  <button type="submit">Register</button>
-                </form>
-              </div>
-        </div>
-        <br/><br/>
-
-    </Layout>
-   
-   </>)
-
+                <div id="feedback-form">
+                    <h2 className="header">Service Booking Form</h2>
+                    <div>
+                        <form action="https://formspree.io/f/meqwdjkz" method="POST">
+                            <label className="pure-material-textfield-standard">
+                                <input type="name" name="name" placeholder="Your Name" required />
+                            </label>
+                            <label className="pure-material-textfield-standard">
+                                <input type="email" name="email" placeholder="Your Email" required />
+                            </label>
+                            <label className="pure-material-textfield-standard">
+                                <input type="tel" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Your Mobile Number" required />
+                            </label>
+                        
+                            <label className="pure-material-textfield-standard">
+                                <input type="name" name="address" placeholder=" Address" required />
+                            </label><br></br>
+                            <label className="pure-material-textfield-standard">
+                                <input type="date" name="date" required />
+                            </label>
+                            <label className="pure-material-textfield-standard">
+                                <input type="time" name="time" required />
+                            </label>
+                            <button type="submit">Book Service</button>
+                        </form>
+                    </div>
+                </div>
+                <br /><br />
+            </Layout>
+        </>
+    )
 }
