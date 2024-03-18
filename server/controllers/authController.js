5c12f96b3a595d132182285c2bbc1c96255eb0fa
vithyawessalini <118716389+vithyawessalini@users.noreply.github.com>
2024-03-18 22:43:22 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -353,41 +353,6 @@ export const updateProfileController = async (req, res) => {
   }
 };
 
-//orders
-export const getOrdersController = async (req, res) => {
-  try {
-    const orders = await orderModel
-      .find({ buyer: req.user._id })
-      .populate("products", "-photo")
-      .populate("buyer", "name");
-    res.json(orders);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({
-      success: false,
-      message: "Error WHile Geting Orders",
-      error,
-    });
-  }
-};
-//orders
-export const getAllOrdersController = async (req, res) => {
-  try {
-    const orders = await orderModel
-      .find({})
-      .populate("products", "-photo")
-      .populate("buyer", "name")
-      .sort({ createdAt: "-1" });
-    res.json(orders);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({
-      success: false,
-      message: "Error WHile Geting Orders",
-      error,
-    });
-  }
-};
 
 //order status
 export const orderStatusController = async (req, res) => {
